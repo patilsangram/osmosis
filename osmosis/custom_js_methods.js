5f6b9f264ffdc768221254732c7e7d7590b0815e
patilsangram <sangram.p@indictranstech.com>
2015-11-16 13:56:07 +0530

    added buyback item

--- osmosis/custom_js_methods.js
+++ osmosis/custom_js_methods.js
@@ -1,5 +1,22 @@
-frappe.ui.form.on("Lead","onload",function(frm){
-	console.log("hiii")
-	frm.date=frappe.datetime.get_today();
-	refresh_field("date")
-})
+frappe.ui.form.on("Sales Order", "refresh", function(frm) {
+		if (cur_frm.doc.docstatus===0) {																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												
+			cur_frm.add_custom_button(__('Extra Sales Order'), make_extra_sales_order);
+		}
+
+});
+
+
+make_extra_sales_order = function(btn) {
+        var eso = frappe.model.make_new_doc_and_get_name('Sales Order');
+        eso = locals['Sales Order'][eso];
+        eso.project_title = cur_frm.doc.project_title;
+        eso.customer = cur_frm.doc.customer;
+        eso.parent_sales_order = cur_frm.doc.name;
+    
+        loaddoc('Sales Order', eso.name);
+
+    }
+    
+
+
+
