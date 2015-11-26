7e0c8d17c50dd7ccf980987e66ebae853671a547
pitambar <pitambar.hatwar@gmail.com>
2015-11-26 11:10:42 +0530

    changed view of buyback item and osmosis app

---
+++ osmosis/config/osmosis.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Documents"),
+			"icon": "icon-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Nature of Complaint",
+					"description": _("Complaints details."),
+				},
+				{
+					"type": "doctype",
+					"name": "Tool Management",
+					"description": _("Tools work and transction."),
+				},
+			]	
+		},
+		{
+			"label":_("Masters"),
+			"icon":"icon-star",
+			"items": [
+					{
+					"type":"doctype",
+					"name":"Nature of Work",
+					"description":_("Work details."),
+					},
+					{
+					"type":"doctype",
+					"name":"Suburb",
+					"description":_("List of suburb."),
+					},
+				]
+		},
+		{	
+			"label":_("Setup"),
+			"icon":"icon-star",
+			"items": [
+				{
+				"type":"doctype",
+				"name":"Osmosis Configurations",
+				"description":_("Tax template for Buyback Item."),
+				}
+			]
+		},
+	]
