6f2d3f091469693186bc7c78439c1ad39127dc4b
patilsangram <sangram.p@indictranstech.com>
2016-04-22 18:48:26 +0530

    allow import to suburb

---
+++ osmosis/osmosis/doctype/nature_of_work/nature_of_work.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, osmosis and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Nature of Work', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ osmosis/osmosis/doctype/suburb/suburb.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, osmosis and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Suburb', {
+	refresh: function(frm) {
+
+	}
+});
